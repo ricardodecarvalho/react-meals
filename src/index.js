c499b4db8624066c536ff75deb8483d05816b7e7
Ricardo de Carvalho <skapitao@gmail.com>
2018-02-16 10:33:41 -0200

    first commit - project complete

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import {createStore, applyMiddleware, compose } from 'redux';
+import reducer from './reducers'
+import {Provider} from 'react-redux';
+import logger from 'redux-logger'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+    reducer,
+    composeEnhancers(
+      applyMiddleware(logger)
+    )
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, document.getElementById('root'));
+registerServiceWorker();
